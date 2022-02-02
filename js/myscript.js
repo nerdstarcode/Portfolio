5d22ebad456f395b7172c03ec7fb96d609139ae1
nerdstarcode <sthivendev@gmail.com>
2022-02-02 12:19:25 -0300

    adicionada funçoes no menu mobile

--- js/myscript.js
+++ js/myscript.js
@@ -2,8 +2,13 @@ const MenubtnArea = document.querySelector(".MenuPush");
 const Header = document.querySelector("header");
 MenubtnArea.addEventListener("click", () => {
     if (Header.classList.contains('active') == true){
-        Header.classList.remove('active')
+        Header.classList.remove('active');
     }else{
         Header.classList.add('active');
     }
 });
+let MenuClick = function(){
+if (Header.classList.contains('active') == true){
+    Header.classList.remove('active');
+}
+}
